4e8882ea881890c02c7ec3bb469a1abdf1279345
dhanirhama <dhanirhama2001@gmail.com>
2023-05-10 12:07:19 +0700

    menambahkan fitur search form

--- js/script.js
+++ js/script.js
@@ -1,15 +1,30 @@
-// Toggle class Active
-const navbarNav = document.querySelector('.navbar-nav');
+// Toggle class Active untuk hamburger menu
+const navbarNav = document.querySelector(".navbar-nav");
 // ketika menu diklik
-document.querySelector('#menu-list').onclick = () => {
-    navbarNav.classList.toggle('active');
+document.querySelector("#menu-list").onclick = () => {
+  navbarNav.classList.toggle("active");
 };
 
-//kilk diluar sidebar untuk menghilangkan navbar
-const Hamburger = document.querySelector('#menu-list');
+// toggle class active untuk toggle search-form
+const searchForm = document.querySelector(".search-form");
+const searchBox = document.querySelector("#search-box");
 
-document.addEventListener('click', function (e) {
-    if (!Hamburger.contains(e.target) && !navbarNav.contains(e.target)) {
-        navbarNav.classList.remove('active');
-    }
+document.querySelector("#search-button").onclick = (e) => {
+  searchForm.classList.toggle("active");
+  searchBox.focus();
+  e.preventDefault();
+};
+
+//kilk diluar elemen
+const Hm = document.querySelector("#menu-list");
+const sb = document.querySelector("#search-button");
+
+document.addEventListener("click", function (e) {
+  if (!Hm.contains(e.target) && !navbarNav.contains(e.target)) {
+    navbarNav.classList.remove("active");
+  }
+
+  if (!sb.contains(e.target) && !searchForm.contains(e.target)) {
+    searchForm.classList.remove("active");
+  }
 });
